e2e92489dae152b0b994b457e68d700463fa56f9
r-grg <rabingrg1010@gmail.com>
2021-12-28 22:42:20 +0100

    Anopheles level base created

---
+++ Survive the Consequences/Assets/Scripts/BaseDoorAutomation.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseDoorAutomation : MonoBehaviour
+{
+    public GameObject movingDoor;
+
+    public float maximumOpening = 5f;
+    public float maximumClosing = -0.77f;
+
+    public float movementSpeed = 3f;
+
+    bool playerIsHere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerIsHere = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsHere && movingDoor.CompareTag("baseDoor_x"))
+        {
+            if (movingDoor.transform.position.x < maximumOpening)
+            {
+                movingDoor.transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f);
+            }
+        }
+        else if (!playerIsHere && movingDoor.CompareTag("baseDoor_x"))
+        {
+            if (movingDoor.transform.position.x > maximumClosing)
+            {
+                movingDoor.transform.Translate(-movementSpeed * Time.deltaTime, 0f, 0f);
+            }
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            playerIsHere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            playerIsHere = false;
+        }
+    }
+}
---
+++ Survive the Consequences/Assets/Scripts/MouseDirectionController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class MouseDirectionController : MonoBehaviour
+{
+    PlayerActions inputActions;
+
+    public Transform target;
+    Vector3 offset;
+    Vector2 mousePosition;
+
+    [SerializeField]
+    float mouseSensitivity = 5.0f;
+
+    private void Awake()
+    {
+        inputActions = new PlayerActions();
+        inputActions.PlayerControls.Look.performed += MouseMovement;
+    }
+
+    private void Start()
+    {
+        offset = target.position - transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void LateUpdate()
+    {
+        MouseRotate();
+    }
+    private void MouseMovement(InputAction.CallbackContext callbackContext)
+    {
+        mousePosition = callbackContext.ReadValue<Vector2>();
+    }
+    private void MouseRotate()
+    {
+        mousePosition.x = mousePosition.x * mouseSensitivity * Time.deltaTime;
+        target.Rotate(0, mousePosition.x, 0);
+
+        float desiredYAngle = target.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+    }
+  
+    private void OnEnable()
+    {
+        inputActions.PlayerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.PlayerControls.Disable();
+    }
+
+}
